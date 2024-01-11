345584189670484158c5bfa573abbb3f323138a2
Rocha <abynerrochab@gmail.com>
2024-01-11 17:31:37 +0000

    History

--- mobile/app/settings/history.tsx
+++ mobile/app/settings/history.tsx
@@ -1,39 +1,40 @@
 import { View, Text, ScrollView } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { History } from '../../services/workouts'
 import { useAuth } from '../../contexts/Auth/AuthContext'
 import { useQuery } from 'react-query'
 import Loading from '../loading'
 import { Feather } from '@expo/vector-icons'
+import Button from '../../components/Button'
 
 export default function WorkoutHistory() {
   const { user } = useAuth()
-  const [isShowing, setIsShowing] 
+  const [isShowing, setIsShowing] = useState<boolean[]>([])
 
   const { data, isFetching, isLoading, error } = useQuery({
     queryKey: '@fetchHistory',
-    queryFn: fetchHistory
+    queryFn: async () => {
+      const historyData = await History.getAll(user?._id!)
+
+      return historyData
+    }
   })
 
-  async function fetchHistory() {
-    const historyData = await History.getAll(user?._id!)
+  useEffect(() => {
+    data?.forEach(() => {
+      setIsShowing((v) => [...v, false])
+    })
 
-    return historyData
-  }
+  }, [])
 
-  if(isLoading || isFetching) return <Loading/>
+  if (isLoading || isFetching) return <Loading />
 
   if (error || !data) return <View className='h-[80%] w-full items-center justify-center bg-neutral-50 flex flex-col space-y-2'>
     <Feather name='x-circle' color='red' size={50} />
     <Text className='text-md text-red-500'>Não foi possivel realizar esta ação neste momento</Text>
   </View>
 
-  useEffect(() => {
-    for(let i = 0; i < data.length; i++) {
 
-    }
-  }, [])
-  
 
   return (
     <View className='p-3'>
@@ -41,16 +42,21 @@ export default function WorkoutHistory() {
       <View className='my-4' />
       <ScrollView className=''>
         {data.map((history, idx) => {
-          return <View key={idx} className='flex-row space-x-1'>
-            <View className='flex-row items-center justify-between w-[92%]'>
-              <Text className='text-xl'>{history.workout.name}</Text>
-              <Text className='text-right'>{new Date(history.date).toLocaleDateString('pt-PT')}</Text>
-            </View>
-            <View>
-              <Feather name='chevron-down' size={28} />
+          return <View key={idx} className='flex-col'>
+            <View className='flex-row space-x-1'>
+              <View className='flex-row items-center justify-between w-[92%]'>
+                <Text className='text-xl'>{history.workout.name}</Text>
+                <Text className='text-right'>{new Date(history.date).toLocaleDateString('pt-PT')}</Text>
+              </View>
+              <View>
+                <Feather name={isShowing[idx] ? 'chevron-up' : 'chevron-down'} size={28} />
+              </View>
             </View>
 
-            {}
+            {isShowing[idx] && <View className='items-center justify-center py-4 px-2'>
+             <Button>Repetir treino</Button> 
+            </View>
+            }
           </View>
         })}
       </ScrollView>
