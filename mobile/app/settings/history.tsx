2d1a43b91254f305eefe6178f738694efb56d369
Rocha <abynerrochab@gmail.com>
2024-01-12 12:57:44 +0000

    History

--- mobile/app/settings/history.tsx
+++ mobile/app/settings/history.tsx
@@ -1,17 +1,41 @@
-import { View, Text, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, ScrollView, Pressable, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { History } from '../../services/workouts'
 import { useAuth } from '../../contexts/Auth/AuthContext'
 import { useQuery } from 'react-query'
 import Loading from '../loading'
 import { Feather } from '@expo/vector-icons'
-import Button from '../../components/Button'
+import AnimatedAccordion from "@dev-event/react-native-accordion";
+import { router } from 'expo-router'
+import { MotiText, MotiView, useAnimationState, useDynamicAnimation } from 'moti'
 
 export default function WorkoutHistory() {
   const { user } = useAuth()
   const [isShowing, setIsShowing] = useState<boolean[]>([])
+  const [scrollPos, setScrollPos] = useState(0)
+  const [scrollHeight, setScrollHeight] = useState(0)
 
-  const { data, isFetching, isLoading, error } = useQuery({
+  const data: { name: string, date: Date }[] = [
+    { name: 'Teste 1', date: new Date() },
+    { name: 'Teste 2', date: new Date() },
+    { name: 'Teste 3', date: new Date() },
+    { name: 'Teste 4', date: new Date() },
+    { name: 'Teste 5', date: new Date() },
+    { name: 'Teste 6', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+    { name: 'Teste 7', date: new Date() },
+  ]
+
+  const textAnimation = useDynamicAnimation(() => ({
+    scale: 1
+  }))
+
+  const { isFetching, isLoading, error } = useQuery({
     queryKey: '@fetchHistory',
     queryFn: async () => {
       const historyData = await History.getAll(user?._id!)
@@ -21,10 +45,12 @@ export default function WorkoutHistory() {
   })
 
   useEffect(() => {
-    data?.forEach(() => {
-      setIsShowing((v) => [...v, false])
+    let states: boolean[] = []
+    data?.forEach((v) => {
+      states.push(false)
     })
 
+    setIsShowing(states)
   }, [])
 
   if (isLoading || isFetching) return <Loading />
@@ -34,29 +60,43 @@ export default function WorkoutHistory() {
     <Text className='text-md text-red-500'>Não foi possivel realizar esta ação neste momento</Text>
   </View>
 
-
+  function handleScroll(event: NativeSyntheticEvent<NativeScrollEvent>) {
+    const { y } = event.nativeEvent.contentOffset
+    setScrollPos(y)
+  }
 
   return (
-    <View className='p-3'>
-      <Text className='text-4xl font-bold'>Histórico</Text>
-      <View className='my-4' />
-      <ScrollView className=''>
+    <View className='flex-1'>
+      <MotiView
+        state={textAnimation}
+      >
+        <Text className='m-4 text-4xl font-bold'>
+          Histórico
+        </Text>
+      </MotiView>
+      <ScrollView
+        className='mb-9'
+        onScroll={handleScroll}
+        onLayout={(e) => {
+          setScrollHeight(e.nativeEvent.layout.height)
+        }}
+      >
         {data.map((history, idx) => {
-          return <View key={idx} className='flex-col'>
-            <View className='flex-row space-x-1'>
-              <View className='flex-row items-center justify-between w-[92%]'>
-                <Text className='text-xl'>{history.workout.name}</Text>
+          return <View key={idx} className='flex-col w-fulç h-20'>
+            <Pressable
+              cancelable
+              className='flex-row space-x-1 w-full h-full'
+            // onPressIn={() => router.push(`/workout/${history.workout._id}`)}
+            >
+              <View className='flex-row items-center justify-between w-[100%] p-2'>
+                <Text className='text-lg'>{history.name}</Text>
                 <Text className='text-right'>{new Date(history.date).toLocaleDateString('pt-PT')}</Text>
               </View>
-              <View>
-                <Feather name={isShowing[idx] ? 'chevron-up' : 'chevron-down'} size={28} />
-              </View>
-            </View>
+            </Pressable>
 
-            {isShowing[idx] && <View className='items-center justify-center py-4 px-2'>
-             <Button>Repetir treino</Button> 
-            </View>
-            }
+            <View
+              className='h-[1] w-full bg-neutral-300'
+            />
           </View>
         })}
       </ScrollView>
